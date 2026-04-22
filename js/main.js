4a437f907cb8459932348c44c7cb5dd23f41c59a
Vishalr2312 <vishal@renderinfotech.com>
2026-04-22 11:56:29 +0530

    last slide original video update

--- js/main.js
+++ js/main.js
@@ -363,8 +363,8 @@
   if (heroVideo && heroSource) {
     const getHeroVideoSrc = () =>
       mobileHeroVideoQuery.matches
-        ? "assets/images/regenerative-loads-mobile-video.mp4"
-        : "assets/images/regenerative-loads-video.mp4";
+        ? "assets/images/regenerative-loads-mobile-org.mp4"
+        : "assets/images/regenerative-loads-org.mp4";
 
     const setVideoSource = () => {
       const nextSrc = getHeroVideoSrc();
